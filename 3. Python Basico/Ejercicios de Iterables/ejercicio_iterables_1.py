b4b4af00c77fdc7a488756da63cfe5d2585da566
Javier Solis <83131409+ja15solis@users.noreply.github.com>
2026-02-26 21:49:49 -0600

    Delete 3. Python Basico directory

--- 3. Python Basico/Ejercicios de Iterables/ejercicio_iterables_1.py
+++
@@ -1,7 +0,0 @@
-albums = ["Bocanada","MTV Unplugged in New York","I'm with you", "The Color and the Shape","Meteora"]
-artist = ["Gustavo Cerati","Nirvana","Red Hot Chili Peppers","Foo Fighters","Linkin Park"]
-
-print("Album and artist: ")
-
-for index, album in enumerate(albums):
-    print(f'{album} - {artist[index]}' )
--- 3. Python Basico/Ejercicios de Sintaxis/ejercicio_de_sintaxis_3.py
+++
@@ -1,9 +0,0 @@
-import random
-secret_num = random.randint(1, 10)
-
-user_input = int(input("Guess the secret number from 1 to 10: "))
-
-while user_input != secret_num :
-    user_input = int(input("Wrong number, try again: "))
-
-print( f'Right, the correct number is {secret_num}')
