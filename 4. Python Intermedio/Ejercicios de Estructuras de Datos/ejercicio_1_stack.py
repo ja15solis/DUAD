31c62780b37825d3f683a972e3e77a76c4d0cecb
Javier Solis <ja15.solis@gmail.com>
2026-03-24 20:23:47 -0600

    Corrections in stack and deque structure, and fixing the 'print_structure' method

--- 4. Python Intermedio/Ejercicios de Estructuras de Datos/ejercicio_1_stack.py
+++ 4. Python Intermedio/Ejercicios de Estructuras de Datos/ejercicio_1_stack.py
@@ -2,19 +2,21 @@ class Node:
     data: str
     next: "Node"
 
-    def __init__(self, data, next=None):
+    def __init__(self, data):
         self.data = data
-        self.next = next
+        self.next = None
 
 class Stack:
     head: Node
 
-    def __init__(self, head):
-        self.head = head
+    def __init__(self):
+        self.head = None
 
     def print_structure(self):
         current_node = self.head
-
+        if not current_node:
+            print("Empty Deque")
+            return
         while current_node is not None:
             print(current_node.data)
             current_node = current_node.next
@@ -22,16 +24,19 @@ class Stack:
     def push(self, new_node):
         current_node = new_node
         current_node.next = self.head
-        while current_node.next is not None:
-            current_node = current_node.next
+        #not needed because the nodes are already connected 
+        #while current_node.next is not None:
+            #current_node = current_node.next
         self.head = new_node
 
     def pop(self):
         if self.head:
             self.head = self.head.next
 
+
+my_stack = Stack()
 first_node = Node("First")
-my_stack = Stack(first_node)
+my_stack.push(first_node)
 # my_stack.print_structure()
 
 second_node = Node("Second")
@@ -45,28 +50,23 @@ my_stack.push(forth)
 
 my_stack.print_structure()
 
-print("POP")
+print("-> POP")
 
 my_stack.pop()
 my_stack.print_structure()
 
-print("POP")
+print("-> POP")
 
 my_stack.pop()
 my_stack.print_structure()
 
-print("POP")
+print("-> POP")
 
 my_stack.pop()
 my_stack.print_structure()
 
-print("POP")
+print("-> POP")
 
 my_stack.pop()
 my_stack.print_structure()
 
-
-print("POP")
-
-my_stack.pop()
-my_stack.print_structure()
--- 4. Python Intermedio/Ejercicios de Estructuras de Datos/ejercicio_2_double_ended_queue.py
+++ 4. Python Intermedio/Ejercicios de Estructuras de Datos/ejercicio_2_double_ended_queue.py
@@ -1,54 +1,74 @@
 class Node:
     data: str
     next: "Node"
+    previous: "Node"
 
-    def __init__(self, data, next=None):
+    def __init__(self, data):
         self.data = data
-        self.next = next
+        self.next = None
+        self.previous = None
 
 class DoubleEndedQueue:
     head: Node
-
-    def __init__(self, head):
-        self.head = head
+    def __init__(self):
+        self.head = None
+        self.last = None
 
     def print_structure(self):
         current_node = self.head
-
-        while current_node is not None:
+        if not current_node:
+            print("Empty Deque")
+            return
+        while current_node.next is not None:
             print(current_node.data)
             current_node = current_node.next
 
     def push_left(self, new_node):
         current_node = new_node
-        current_node.next = self.head
-        while current_node.next is not None:
-            current_node = current_node.next
-        self.head = new_node
+        new_node.next = None
+        new_node.previous = None
+        if not self.head:
+            self.head = current_node
+            self.last = current_node
+        else:
+            current_node.next = self.head
+            self.head.previous = current_node
+            self.head = current_node
 
     def pop_left(self):
-        if self.head:
+        if not self.head:
+            return
+        if self.head == self.last: #one element
+            self.head = None
+            self.last = None
+        else:
             self.head = self.head.next
+            self.head.previous = None
     
     def push_right(self, new_node):
-        current_node = self.head
-
-        while current_node.next is not None:
-            current_node = current_node.next
-
-        current_node.next = new_node
+        current_node = new_node
+        new_node.next = None
+        new_node.previous = None
+        if not self.last:
+            self.last = current_node
+            self.head = current_node
+        else:
+            self.last.next = current_node
+            current_node.previous = self.last
+            self.last = current_node
     
     def pop_right(self):
-        current_node = self.head
-        while current_node.next is not None:
-            next_node = current_node.next
-            if next_node.next is not None:
-                current_node = next_node
-            else:
-                current_node.next = None
-
+        if not self.last:
+            return
+        if self.head == self.last: #one element
+            self.head = None
+            self.last = None
+        else:
+            self.last = self.last.previous
+            self.last.next = None
+my_double_e_queue = DoubleEndedQueue()
 first_node = Node("First")
-my_double_e_queue = DoubleEndedQueue(first_node)
+my_double_e_queue.push_right(first_node)
 # my_double_e_queue
 #.print_structure()
 print("-"*30)
@@ -76,5 +96,5 @@ my_double_e_queue.print_structure()
 
 print("-"*30)
 print("-> push_left")
-my_double_e_queue.push_left(first_node)
+my_double_e_queue.push_left(first_node) ##I have to clear the next and previous nodes
 my_double_e_queue.print_structure()
