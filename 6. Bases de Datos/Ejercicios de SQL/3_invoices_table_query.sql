ed5222bb06e3be75963098748e30f58efc9fa0fa
Javier Solis <ja15.solis@gmail.com>
2026-05-20 09:16:55 -0600

    Corrections in SQLite exercises

--- 6. Bases de Datos/Ejercicios de SQL/3_invoices_table_query.sql
+++ 6. Bases de Datos/Ejercicios de SQL/3_invoices_table_query.sql
@@ -2,8 +2,8 @@
 CREATE TABLE invoices (
 id INTEGER PRIMARY KEY, --"INTEGER" To be added automatically 
 shopping_cart_id INT REFERENCES shopping_cart(id),
-invoice_number BIGINT,
+invoice_number BIGINT UNIQUE,
 purchase_date TEXT DEFAULT CURRENT_DATE,
 buyer_email TEXT NOT NULL,
-total_ammount FLOAT NOT NULL DEFAULT 0
+total_amount FLOAT NOT NULL DEFAULT 0
 );
--- 6. Bases de Datos/Ejercicios de SQL/4_select_exercises.sql
+++ 6. Bases de Datos/Ejercicios de SQL/4_select_exercises.sql
@@ -1,4 +1,4 @@
-1
+--1
 
 SELECT * FROM products
 ;
